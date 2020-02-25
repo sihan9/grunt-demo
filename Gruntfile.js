2a894799f624c1a72c686f59fd33c88f4159d43e
sihan9 <1060192071@qq.com>
2020-02-25 17:50:20 +0800

    csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,16 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    htmlhint: {
+    csslint: {
       options: {
-        htmlhintrc: '.htmlhintrc'
+        csslintrc: '.csslintrc'
       },
-      src: ['*.html']
+      src: ['*.css']
     }
   });
 
-  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
 
-  grunt.registerTask('default', ['htmlhint']);
+  grunt.registerTask('default', ['csslint']);
 };
 
  
